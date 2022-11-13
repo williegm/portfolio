71aa4d2d5b275d92b0fbfe7aad5a0e8808d271d5
williegm <grimmettewilliam@gmail.com>
2022-11-13 06:28:00 -0500

    feat: add certificate

--- data/content/designs.ts
+++ data/content/designs.ts
@@ -1,33 +1,13 @@
-type Design = {
+type Certificate = {
   label: string;
   img: string;
   link: string;
 };
 
-export const designs: Design[] = [
+export const designs: Certificate[] = [
   {
-    label: "",
-    img: "/static/designs/demo.png",
-    link: "",
-  },
-  {
-    label: "",
-    img: "/static/designs/demo.png",
-    link: "",
-  },
-  {
-    label: "",
-    img: "/static/designs/demo.png",
-    link: "",
-  },
-  {
-    label: "",
-    img: "/static/designs/demo.png",
-    link: "",
-  },
-  {
-    label: "",
-    img: "/static/designs/demo.png",
-    link: "",
+    label: "React - Triplebyte",
+    img: "/static/certificates/triplebyte-react.png",
+    link: "https://triplebyte.com/candidates/certificate",
   },
 ];
