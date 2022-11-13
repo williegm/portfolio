2ed7105c03f506dea93b8e616c079a978c153ace
williegm <grimmettewilliam@gmail.com>
2022-11-13 06:44:41 -0500

    chore: update coffee content

--- data/content/support.ts
+++ data/content/support.ts
@@ -6,17 +6,17 @@ type Testimonial = {
 
 export const testimonials: Testimonial[] = [
   {
-    quote: "Love your work. Let's have a cup of coffee together.",
+    quote: "Love your work. Let's have a cup of coffee together. ☕️ = 0.01 eth",
     count: 0.01,
     custom: false,
   },
   {
-    quote: "Love your work. Let's have a cup of tea together.",
+    quote: "Let's have a cup of tea together. ☕️ ✖️ 10 = 0.1 eth",
     count: 0.1,
     custom: false,
   },
   {
-    quote: "Contact me for any questions and support.",
+    quote: "Contact me for any questions and support. ☕️ ✖️ ✉️ = ?",
     count: 0.1,
     custom: true,
   },
